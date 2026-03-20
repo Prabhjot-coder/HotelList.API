3e1ec3c4fb10528639e0d1ac9760dfa2ded39a19
Prabhjot-coder <68675827+Prabhjot-coder@users.noreply.github.com>
2026-03-20 22:06:04 +0530

    feat: StudentRepository

--- Repositories/StudentRepository.cs
+++ Repositories/StudentRepository.cs
@@ -2,9 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using WebApplication4.Data;
 using WebApplication4.Models;
 namespace WebApplication4.Repositories;
+
 public class StudentRepository : GenericRepository<Student>, IStudentRepository
 {
     public StudentRepository(SchoolContext context) : base(context) { }
+
     public async Task<IEnumerable<Student>> GetStudentsByCourseAsync(int courseId) =>
         await _dbSet.AsNoTracking().Where(s => s.CourseId == courseId).ToListAsync();
 }
