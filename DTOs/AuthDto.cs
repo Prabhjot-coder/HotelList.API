404b0b3ff2999bdc30dd99eb4059bddd52af95db
Prabhjot-coder <68675827+Prabhjot-coder@users.noreply.github.com>
2026-03-20 22:06:45 +0530

    feat: AuthDto

---
+++ DTOs/AuthDto.cs
@@ -0,0 +1,5 @@
+namespace WebApplication4.DTOs;
+
+public record RegisterDto(string FirstName, string LastName, string Email, string Password);
+public record LoginDto(string Email, string Password);
+public record AuthResponseDto(string Token, string Email, string FullName, DateTime ExpiresAt);
