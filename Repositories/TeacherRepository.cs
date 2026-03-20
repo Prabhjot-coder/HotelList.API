110e95d72bfaf9ad1370131c57a9fe68a1bcf11d
Prabhjot-coder <68675827+Prabhjot-coder@users.noreply.github.com>
2026-03-20 22:05:40 +0530

    feat: TeacherRepository

--- Repositories/TeacherRepository.cs
+++ Repositories/TeacherRepository.cs
@@ -2,9 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using WebApplication4.Data;
 using WebApplication4.Models;
 namespace WebApplication4.Repositories;
+
 public class TeacherRepository : GenericRepository<Teacher>, ITeacherRepository
 {
     public TeacherRepository(SchoolContext context) : base(context) { }
+
     public async Task<IEnumerable<Teacher>> GetTeachersByDepartmentAsync(int departmentId) =>
         await _dbSet.AsNoTracking().Where(t => t.DepartmentId == departmentId).ToListAsync();
 }
