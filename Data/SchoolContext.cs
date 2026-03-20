a0c2f665a24b092d0dcae4de80656f1681d2d2cf
Prabhjot-coder <68675827+Prabhjot-coder@users.noreply.github.com>
2026-03-20 23:07:45 +0530

    fix: guard OnConfiguring so InMemory DB works in tests

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using WebApplication4.Models;
@@ -25,23 +25,26 @@ public partial class SchoolContext : DbContext
     public virtual DbSet<Teacher> Teachers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ManavMangal;Integrated Security=True;");
+    {
+        // Only use fallback connection string if no options were injected via DI
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ManavMangal;Integrated Security=True;");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Course>(entity =>
         {
             entity.HasKey(e => e.CourseId).HasName("PK__COURSES__C92D71A761A62BF3");
-
             entity.Property(e => e.IsActive).HasDefaultValue(true);
-
             entity.HasOne(d => d.Teacher).WithMany(p => p.Courses).HasConstraintName("FK__COURSES__Teacher__2C3393D0");
         });
 
         modelBuilder.Entity<Department>(entity =>
         {
             entity.HasKey(e => e.DepartmentId).HasName("PK__Departme__B2079BED8149F0FC");
-
             entity.Property(e => e.CreatedDate).HasDefaultValueSql("(getdate())");
             entity.Property(e => e.IsActive).HasDefaultValue(true);
         });
@@ -49,18 +52,14 @@ public partial class SchoolContext : DbContext
         modelBuilder.Entity<Student>(entity =>
         {
             entity.HasKey(e => e.StudentId).HasName("PK__STUDENTS__32C52B9947B33C61");
-
             entity.Property(e => e.IsActive).HasDefaultValue(true);
-
             entity.HasOne(d => d.Course).WithMany(p => p.Students).HasConstraintName("FK__STUDENTS__Course__35BCFE0A");
         });
 
         modelBuilder.Entity<Teacher>(entity =>
         {
             entity.HasKey(e => e.TeacherId).HasName("PK__Teachers__EDF259644AEE8419");
-
             entity.Property(e => e.IsActive).HasDefaultValue(true);
-
             entity.HasOne(d => d.Department).WithMany(p => p.Teachers).HasConstraintName("FK__Teachers__Depart__286302EC");
         });
 
