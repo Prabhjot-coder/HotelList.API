631ab15664b64da84989f2cfc9a7870f446d89f8
Prabhjot-coder <68675827+Prabhjot-coder@users.noreply.github.com>
2026-03-20 22:04:59 +0530

    feat: IStudentRepository

--- Repositories/IStudentRepository.cs
+++ Repositories/IStudentRepository.cs
@@ -1,5 +1,6 @@
 using WebApplication4.Models;
 namespace WebApplication4.Repositories;
+
 public interface IStudentRepository : IGenericRepository<Student>
 {
     Task<IEnumerable<Student>> GetStudentsByCourseAsync(int courseId);
