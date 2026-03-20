2a4db92f992b4c0ec6fcb14a60fc6c2137778989
Prabhjot-coder <68675827+Prabhjot-coder@users.noreply.github.com>
2026-03-20 22:05:27 +0530

    feat: DepartmentRepository

--- Repositories/DepartmentRepository.cs
+++ Repositories/DepartmentRepository.cs
@@ -2,9 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using WebApplication4.Data;
 using WebApplication4.Models;
 namespace WebApplication4.Repositories;
+
 public class DepartmentRepository : GenericRepository<Department>, IDepartmentRepository
 {
     public DepartmentRepository(SchoolContext context) : base(context) { }
+
     public async Task<IEnumerable<Department>> GetActiveDepartments() =>
         await _dbSet.AsNoTracking().Where(d => d.IsActive == true).ToListAsync();
 }
