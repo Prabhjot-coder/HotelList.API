8df786ec7f6c7b59f9c2bbb09a0389cc1195fdfe
Prabhjot-coder <68675827+Prabhjot-coder@users.noreply.github.com>
2026-03-20 22:03:06 +0530

    feat: IGenericRepository interface

---
+++ Repositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+using System.Linq.Expressions;
+namespace WebApplication4.Repositories;
+public interface IGenericRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> expression);
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(int id);
+    Task<bool> ExistsAsync(int id);
+    Task<int> CountAsync();
+}
